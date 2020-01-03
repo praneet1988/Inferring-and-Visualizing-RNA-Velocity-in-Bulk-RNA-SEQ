dfba7c99be9dd6e6a36f27cfef22d6ae87456ec3
Praneet Chaturvedi <55851416+praneet1988@users.noreply.github.com>
2020-01-03 13:48:17 -0500

    Add files via upload

---
+++ LoomCombine.py
@@ -0,0 +1,6 @@
+import loompy
+
+
+
+list = [List of Loom Files]                          ####### list = ['loom1.loom', 'loom2.loom', loom3.loom']                                           
+loompy.combine(list, 'Combined.loom', key="Accession")
